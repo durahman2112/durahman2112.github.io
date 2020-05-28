05f6b274e0751c347759490b792ff8346b85123a
AldiRvn <aldirvn4@gmail.com>
2020-05-28 16:49:56 +0700

    change html.js, & index.html

--- html.js
+++ html.js
@@ -799,6 +799,402 @@ function loadHTML(name) {
             `
             break;
 
+    // reviewfull/
+        case 'reviewfull/cover':
+            return `
+            <div class="cover">
+                <div class="gambar">
+                    <img class="replace" src="reviewfull/tgk.jpg" alt="" srcset="">
+                </div>
+               <img src="blur/blur.png" alt="" class="footer">
+                <div class="white-box"></div>
+                <p class="title">
+                    kimetsu no yaiba
+                </p>
+                <p class="desc">
+                    Kisah para anak manusia yang dijadikan makanan oleh para iblis, berjuang untuk keluar atau 
+                </p>
+                <div class="bg-judul-content"></div>
+                <p class="judul-content">
+                    Review anime
+                </p>
+                <p class="lensa">
+                    lensa.animeme
+                </p>
+                <p class="follow">
+                    follow for more anime content!
+                </p>
+                <p class="admin">
+                    by <span class="author">hoshi</span>
+                </p>
+                <div class="icon">
+                    <img src="reviewfull/icon.png" alt="" srcset="">
+                </div>
+                <div class="linebreak">
+        
+                </div>
+                <div class="bulet">
+                    <img src="reviewfull/bulet.png" alt="">
+                </div>
+                <div class="logo">
+                    <img src="reviewfull/logo.png" alt="" srcset="">
+                </div>
+        
+            </div>
+    
+            <br>
+    
+            <form action="" id="form">
+                <div class="form-group">
+                    <label for="title">Judul Anime</label>
+                    <input type="text" class="form-control" id="title">
+                </div>
+                <div class="form-group">
+                    <label for="desc">Desc Singkat</label>
+                    <input type="text" class="form-control" id="desc">
+                </div>
+                
+                <div class="form-group">
+                    <label for="gambar">Pilih gambar</label>
+                    <input type="file" class="form-control-file" id="gambar">
+                </div>
+                <div class="form-group">
+                    <label for="author">Admin</label>
+                    <input type="test" class="form-control" id="author">
+                </div>
+            </form>
+            `
+            break;
+        case 'reviewfull/detail':
+            return `
+            <div class="cover">
+                <img src="blur/blur-review-detail.png" alt="" class="footer">
+                <div class="gambar-content">
+                    <img class="replace" src="reviewfull/tgk.jpg" alt="" srcset="">
+                </div>
+                <div class="white-box"></div>
+                <p class="title">
+                    kimetsu no yaiba
+                </p>
+                <p class="rilis">
+                    summer 2020
+                </p>
+                <p class="sinopsis">
+                    Meski dalam wabah COVID-19, produksi film terbaru Hayao Mizaki, ‘Kimitachi wa dou Ikiru Ka’ tetap berlanjut dari kabaran sudah berjalan 36 menit, ditargetkan selesai 3 tahun mendatangMeski dalam wabah COVID-19, produksi film terbaru Hayao Mizaki, ‘Kimitachi wa dou Ikiru Ka’ tetap 
+                </p>
+                <div class="node"></div>
+                <p class="episode">12 episode</p>
+                <p class="studio">
+                    kyoto animation
+                </p>
+                <p class="genre">
+                    comedy, drama, slice of life, adventure
+                </p>
+                <div class="bg-judul-content"></div>
+                <p class="judul-content">
+                    Review anime
+                </p>
+                <p class="lensa">
+                    lensa.animeme
+                </p>
+                <p class="follow">
+                    follow for more anime content!
+                </p>
+                <p class="admin">
+                    by <span class="author">hoshi</span>
+                </p>
+                <div class="icon">
+                    <img src="reviewfull/icon.png" alt="" srcset="">
+                </div>
+                <div class="linebreak">
+        
+                </div>
+                <div class="bulet">
+                    <img src="reviewfull/bulet.png" alt="">
+                </div>
+                <div class="logo">
+                    <img src="reviewfull/logo.png" alt="" srcset="">
+                </div>
+        
+            </div>
+    <br>
+    
+            <form action="" id="form">
+                <div class="form-group">
+                    <label for="title">Judul Anime</label>
+                    <input type="text" class="form-control" id="title">
+                </div>
+                <div class="form-group">
+                    <label for="rilis">Musim Rilis</label>
+                    <input type="text" class="form-control" id="rilis">
+                    <small class="form-text text-muted">example : Summer 2020</small>
+                </div>
+                <div class="form-group">
+                    <label for="episode">Jumlah Episode</label>
+                    <input type="text" class="form-control" id="episode">
+                    <small class="form-text text-muted">example : 12 Episode</small>
+                </div>
+                <div class="form-group">
+                    <label for="studio">Studio</label>
+                    <input type="text" class="form-control" id="studio">
+                </div>
+                <div class="form-group">
+                    <label for="genre">Genre</label>
+                    <input type="text" class="form-control" id="genre">
+                </div>
+                <div class="form-group">
+                    <label for="sinopsis">Sinopsis</label>
+                    <textarea class="form-control" id="sinopsis"></textarea>
+                </div>
+                
+                <div class="form-group">
+                    <label for="gambar">Pilih gambar</label>
+                    <input type="file" class="form-control-file" id="gambar">
+                </div>
+                <div class="form-group">
+                    <label for="author">Admin</label>
+                    <input type="test" class="form-control" id="author">
+                </div>
+            </form>
+            `
+            break;
+        case 'reviewfull/score':
+            return `
+            <div class="cover">
+                <div class="gambar">
+                    <img class="replace" src="reviewfull/kny.jpg" alt="" srcset="">
+                </div>
+               <img src="blur/blur-side.png" alt="" class="footer">
+                <div class="white-box"></div>
+                <img src="reviewfull/bg-score.png" alt="" class="bg-score">
+                <p class="score">8.1</p>
+                <p class="title">
+                    yakusoku no neverland
+                </p>
+                <img src="reviewfull/star.png" alt="" class="star">
+                <img src="reviewfull/bg-miniscore.png" alt="" class="bg-story">
+                <p class="story">8.1</p>
+                <p class="pstory">Story</p>
+                <img src="reviewfull/bg-miniscore.png" alt="" class="bg-chara">
+                <p class="chara">8.2</p>
+                <p class="pchara">Chara</p>
+                <img src="reviewfull/bg-miniscore.png" alt="" class="bg-art">
+                <p class="art">8.7</p>
+                <p class="part">Grafis</p>
+                <img src="reviewfull/bg-miniscore.png" alt="" class="bg-sound">
+                <p class="sound">8.6</p>
+                <p class="psound">sound</p>
+                <img src="reviewfull/bg-miniscore.png" alt="" class="bg-enjoy">
+                <p class="enjoy">8.0</p>
+                <p class="penjoy">Enjoy</p>
+                
+                
+                
+                <p class="lensa">
+                    lensa.animeme
+                </p>
+                <p class="follow">
+                    follow for more anime content!
+                </p>
+                <p class="admin">
+                    by <span class="author">hoshi</span>
+                </p>
+                <div class="icon">
+                    <img src="reviewfull/icon.png" alt="" srcset="">
+                </div>
+                <div class="linebreak">
+        
+                </div>
+                <div class="logo">
+                    <img src="reviewfull/logo.png" alt="" srcset="">
+                </div>
+        
+            </div>
+    <br>
+    
+            <form action="" id="form">
+                <div class="form-group">
+                    <label for="title">Judul News</label>
+                    <input type="text" class="form-control" id="title">
+                </div>
+                <div class="form-row">
+                    <div class="form-group col">
+                        <label for="story">Story</label>
+                        <input type="text" class="form-control" id="story">
+                    </div>
+                    <div class="form-group col">
+                        <label for="chara">Chara</label>
+                        <input type="text" class="form-control" id="chara">
+                    </div>
+                    <div class="form-group col">
+                        <label for="art">Grafis</label>
+                        <input type="text" class="form-control" id="art">
+                    </div>
+                    <div class="form-group col">
+                        <label for="sound">Sound</label>
+                        <input type="text" class="form-control" id="sound">
+                    </div>
+                    <div class="form-group col">
+                        <label for="enjoy">Enjoy</label>
+                        <input type="text" class="form-control" id="enjoy">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="score">Nilai Overview (Hasil rata - rata semua)</label>
+                    <input type="text" class="form-control" id="score">
+                </div>
+                
+                <div class="form-group">
+                    <label for="gambar">Pilih gambar</label>
+                    <input type="file" class="form-control-file" id="gambar">
+                </div>
+                <div class="form-group">
+                    <label for="author">Admin</label>
+                    <input type="test" class="form-control" id="author">
+                </div>
+            </form>
+            `
+            break;
+        case 'reviewfull/ss':
+            return `
+            <div class="cover">
+                <div class="gambar">
+                    <img src="gambar.png" alt="" srcset="">
+                </div>
+                <img src="../blur/blur-full.png" alt="" class="footer">
+                <img src="gambar2.jpg" alt="" class="img-1">
+                <img src="gambar2.jpg" alt="" class="img-2">
+                <img src="gambar2.jpg" alt="" class="img-3">
+                <img src="gambar2.jpg" alt="" class="img-4">
+                <img src="gambar2.jpg" alt="" class="img-5">
+                <img src="gambar2.jpg" alt="" class="img-6">
+                <img src="gambar2.jpg" alt="" class="img-7">
+                <img src="gambar2.jpg" alt="" class="img-8">
+                <p class="lensa">
+                    lensa.animeme
+                </p>
+                <p class="follow">
+                    follow for more anime content!
+                </p>
+                <p class="admin">
+                    by <span class="author">hoshi</span>
+                </p>
+                <div class="icon">
+                    <img src="icon.png" alt="" srcset="">
+                </div>
+                <div class="linebreak">
+        
+                </div>
+                <div class="logo">
+                    <img src="logo.png" alt="" srcset="">
+                </div>
+        
+            </div>
+    <br>
+    
+            <form action="" id="form">
+                <div class="form-row">
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 1</label>
+                        <input type="file" class="form-control-file" id="img-1">
+                    </div>
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 2</label>
+                        <input type="file" class="form-control-file" id="img-2">
+                    </div>
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 3</label>
+                        <input type="file" class="form-control-file" id="img-3">
+                    </div>
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 4</label>
+                        <input type="file" class="form-control-file" id="img-4">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 5</label>
+                        <input type="file" class="form-control-file" id="img-5">
+                    </div>
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 6</label>
+                        <input type="file" class="form-control-file" id="img-6">
+                    </div>
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 7</label>
+                        <input type="file" class="form-control-file" id="img-7">
+                    </div>
+                    <div class="form-group col">
+                        <label for="exampleInputEmail1">Pilih gambar 8</label>
+                        <input type="file" class="form-control-file" id="img-8">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Admin</label>
+                    <input type="test" class="form-control" id="author">
+                </div>
+            </form>
+            `
+            break;
+    
+    // reviewpereps/
+        case 'reviewpereps/cover':
+            return `
+            <div class="cover">
+                <div class="gambar">
+                    <img class="replace" src="reviewpereps/gambar.png" alt="" srcset="">
+                </div>
+                <img src="blur/blur.png" alt="" class="footer">
+                <div class="white-box"></div>
+                <p class="title">
+                    Review yakusoku no neverland episode 8
+                </p>
+                <div class="bg-judul-content"></div>
+                <p class="judul-content">
+                    Review singkat
+                </p>
+                <p class="lensa">
+                    lensa.animeme
+                </p>
+                <p class="follow">
+                    follow for more anime content!
+                </p>
+                <p class="admin">
+                    by <span class="author">hoshi</span>
+                </p>
+                <div class="icon">
+                    <img src="reviewpereps/icon.png" alt="" srcset="">
+                </div>
+                <div class="linebreak">
+        
+                </div>
+                <div class="bulet">
+                    <img src="reviewpereps/bulet.png" alt="">
+                </div>
+                <div class="logo">
+                    <img src="reviewpereps/logo.png" alt="" srcset="">
+                </div>
+        
+            </div>
+    <br>
+            <form action="" id="form">
+                <div class="form-group">
+                    <label for="title">Judul</label>
+                    <input type="text" class="form-control" id="title">
+                    <small class="form-text text-muted">example : Review Yakusoku No Neverland Episode 10</small>
+                </div>
+                <div class="form-group">
+                    <label for="gambar">Pilih gambar</label>
+                    <input type="file" class="form-control-file" id="gambar">
+                </div>
+                <div class="form-group">
+                    <label for="author">Admin</label>
+                    <input type="test" class="form-control" id="author">
+                </div>
+            </form>
+            `
+            break;
+
         default:
             break;
     }
