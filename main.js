c329b19fcab2bc8e592115066286fe1cd4a582bf
AldiRvn <aldirvn4@gmail.com>
2020-05-29 10:51:14 +0700

    change main.js

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ function changeTemplate(name) {
     }
     nowTemplate = name
     add_link_css(name)
-    preview.innerHTML = loadHTML(name)
+    // preview.innerHTML = loadHTML(name)
+    includeHTML(name)
     preview.children[0].classList.add('scale')
 
     input_file = document.querySelectorAll("#preview input[type='file']")
@@ -108,3 +109,23 @@ function preview(){
 //             window.saveAs(blob, fileName + '.png')
 //         })
 // }
+function includeHTML(file) {
+    let elmnt, xhttp
+
+    elmnt = document.querySelector('#html-replace')
+    /* Make an HTTP request using the attribute value as the file name: */
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+        if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+        if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+        /* Remove the attribute, and call this function once more: */
+        // elmnt.removeAttribute("w3-include-html");
+        // includeHTML();
+        }
+    }
+    xhttp.open("GET", file + '.html', true);
+    xhttp.send();
+    /* Exit the function: */
+    return;      
+}
